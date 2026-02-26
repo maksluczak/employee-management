df19342434526f6b3ac42ae6a181724fdfea9bfc
maksluczak <maksluczak99@gmail.com>
2026-02-26 15:41:33 +0100

    update token generation

--- employee-management-system/src/main/java/io/github/maksluczak/ems/config/JwtService.java
+++ employee-management-system/src/main/java/io/github/maksluczak/ems/config/JwtService.java
@@ -36,11 +36,11 @@ public class JwtService {
     public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
         return Jwts
                 .builder()
-                .setClaims(extraClaims)
-                .setSubject(userDetails.getUsername())
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
-                .signWith(getSingInKey(), SignatureAlgorithm.HS256)
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .signWith(getSingInKey())
                 .compact();
     }
 
