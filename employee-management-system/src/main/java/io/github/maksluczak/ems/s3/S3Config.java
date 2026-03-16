d6e78d5be442b7f414cb7d6add065994983b1b08
maksluczak <maksluczak99@gmail.com>
2026-03-16 12:15:30 +0100

    create s3 service and configuration, add aws s3 dependency

---
+++ employee-management-system/src/main/java/io/github/maksluczak/ems/s3/S3Config.java
@@ -0,0 +1,17 @@
+package io.github.maksluczak.ems.s3;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.EU_NORTH_1)
+                .build();
+    }
+}
